5ef7cfc7f3ffdf796d4acc0001a7b49fb85fd84a
Sylvain Peigney <sylvain@volpeo.fr>
2014-11-03 16:04:16 +0100

    extended demo

--- models/Book.php
+++ models/Book.php
@@ -24,7 +24,9 @@
       return array(
         "id" => 1,
         "title" => "Alice au pays des merveilles",
-        "description" => "<p>'Perhaps it hasn't one,' Alice ventured to remark.</p><p>'Tut, tut, child!' said the Duchess. 'Everything's got a moral, if only you can find it.' And she squeezed herself up closer to Alice's side as she spoke.</p><p>Alice did not much like keeping so close to her: first, because the Duchess was VERY ugly; and secondly, because she was exactly the right height to rest her chin upon Alice's shoulder, and it was an uncomfortably sharp chin. However, she did not like to be rude, so she bore it as well as she could.</p>"
+        "description" => "<p>'Perhaps it hasn't one,' Alice ventured to remark.</p><p>'Tut, tut, child!' said the Duchess. 'Everything's got a moral, if only you can find it.' And she squeezed herself up closer to Alice's side as she spoke.</p><p>Alice did not much like keeping so close to her: first, because the Duchess was VERY ugly; and secondly, because she was exactly the right height to rest her chin upon Alice's shoulder, and it was an uncomfortably sharp chin. However, she did not like to be rude, so she bore it as well as she could.</p>",
+        "author" => "Lewis Caroll",
+        "remaining" => 3
       );
     }
 
--- views/books/show.php
+++ views/books/show.php
@@ -1,7 +1,27 @@
-<h2><?php echo $this->data['book']['title'] ?></h2>
+<div class="media">
+  <div class="media-left">
+    <img class="img-thumbnail" src="http://i.imgur.com/Ck1nKbE.jpg" alt="Cover <?php echo $this->data['book']['title'] ?>">
+  </div>
+  <div class="media-body">
+    <h2 class="media-heading"><?php echo $this->data['book']['title'] ?> <small>by <?php echo $this->data['book']['author'] ?></small></h2>
 
-<div>
-  <?php echo $this->data['book']['description'] ?>
+    <?php if ($this->data['book']['remaining'] > 1): ?>
+      <p class="alert alert-success">
+        <?php echo $this->data['book']['remaining'] ?> copies remaining
+      </p>
+    <?php elseif ($this->data['book']['remaining'] == 1): ?>
+      <p class="alert alert-warning">
+        Only one copy remaining !
+      </p>
+    <?php else: ?>
+      <p class="alert alert-danger">
+        Warning: no remaining copy !
+      </p>
+    <?php endif ?>
+    <div>
+      <?php echo $this->data['book']['description'] ?>
+    </div>
+  </div>
 </div>
 
 <a href="<?php echo $app->urlFor('root')  ?>">< Back</a>
